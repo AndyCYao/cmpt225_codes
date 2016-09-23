9141420561f64cb2fe92ae763ff2f411afd68635
Andy Yao ubuntu <andyyao3@gmail.com>
2016-09-22 23:52:15 -0700

    Assignment 1, successfully use the getline and peek and eof methods to loop through a redirected textfile

--- Assignment1/gscheck.cpp
+++ Assignment1/gscheck.cpp
@@ -3,24 +3,86 @@ Andy Yao
 301064847
 CMPT 225 Assignment 1
 Sept 21st 2016
+-To Do List
+	generate the correct error message
 */
 
 #include "char_stack.h"
 
-//topics of reading redirected file at here. 
-//http://stackoverflow.com/questions/13559462/using-input-redirection-how-to-read-file-and-get-character-string-in-c
 #include <iostream>
 
+
 int main(int argc, char * argv[]){
-	char* line[250];
+	char line[250]; // 250 because spec sheet detailed max 250 char per line.
 	char c;
+	int c_count;
 	char_stack S;
 
 	std::cout << "Begin of the program \n";
 	
-	while(std::getline(std::cin, line)){
-	//while (std::cin.get(c)){
-		std::cout << "Entered '" << c << "'" << std::endl;
+	while(!std::cin.peek() == std::cin.eof()){
+		std::cin.getline(line, 250);
+		c_count = std::cin.gcount();
+
+		std::cout<< c_count << std::endl;
+		//loop through the line
+		for(int x = 0; x < c_count; x++){
+			c = line[x];
+			//std::cout << c;
+			
+			if (c == '(' || 
+				c == '{' || 
+				c == '['){
+				S.push(c);	
+			}	
+			else if(c == ')'){
+				
+				if(S.empty()){
+					std::cout << "Error, too many " << c;
+				}
+				char l = S.pop();
+				if(l != '('){
+					std::cout << "error, read " << l <<
+					" expected " << "(";
+				}
+			}	
+			else if(c == '}'){
+				
+				if(S.empty()){
+					std::cout << "Error, too many " << c;
+				}
+				char l = S.pop();
+				if(l != '{'){
+					std::cout << "error, read " << l <<
+					" expected " << "{";
+				}
+			}
+			else if(c == ']'){
+				
+				if(S.empty()){
+					std::cout << "Error, too many " << c;
+				}
+				char l = S.pop();
+				if(l != ']'){
+					std::cout << "error, read " << l <<
+					" expected " << "[";
+				}
+			}
+			
+		}
+	}
+
+	if (!S.empty()){
+		c = S.pop();
+		std::cout << "Error, Too many " << c;
+	}
+	std::cout <<"No Errors Found";
+	return 0;
+}
+
+
+
+
 		/*
 		if (c is an opening symbol){
 			S.push(c)
@@ -36,12 +98,3 @@ int main(int argc, char * argv[]){
 			}
 		}
 		*/
-	}
-
-	if (!S.empty()){
-		c = S.pop();
-		//std::cout << error "Too many c";
-	}
-	std::cout <<"No Errors Found";
-	return 0;
-}
